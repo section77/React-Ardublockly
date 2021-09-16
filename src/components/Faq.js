431719f2421ce97ed320247d21ea91fcc8e239cf
Mario Pesch <mario.pesch@uni-muenster.de>
2021-09-16 15:02:10 +0200

    add markdown editor

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,98 +1,124 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Breadcrumbs from './Breadcrumbs';
+import Breadcrumbs from "./Breadcrumbs";
 
-import { withRouter } from 'react-router-dom';
+import { withRouter } from "react-router-dom";
 
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import * as Blockly from 'blockly'
-import ReactMarkdown from 'react-markdown';
-import Container from '@material-ui/core/Container';
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import * as Blockly from "blockly";
+import ReactMarkdown from "react-markdown";
+import Container from "@material-ui/core/Container";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
-import { FaqQuestions } from '../data/faq'
-
+import { FaqQuestions } from "../data/faq";
+import Editor from "rich-markdown-editor";
 
 class Faq extends Component {
+  state = {
+    panel: "",
+    expanded: false,
+    text: "",
+  };
 
-    state = {
-        panel: '',
-        expanded: false
-    }
+  handleChange = (panel) => {
+    this.setState({ panel: this.state.panel === panel ? "" : panel });
+  };
 
+  componentDidMount() {
+    // Ensure that Blockly.setLocale is adopted in the component.
+    // Otherwise, the text will not be displayed until the next update of the component.
 
-    handleChange = (panel) => {
-        this.setState({ panel: this.state.panel === panel ? '' : panel });
-    };
+    window.scrollTo(0, 0);
+    this.forceUpdate();
+  }
 
-    componentDidMount() {
-        // Ensure that Blockly.setLocale is adopted in the component.
-        // Otherwise, the text will not be displayed until the next update of the component.
-
-        window.scrollTo(0, 0)
-        this.forceUpdate();
-    }
-
-    render() {
-        const { panel } = this.state;
-        return (
-            <div>
-                <Breadcrumbs content={[{ link: this.props.location.pathname, title: 'FAQ' }]} />
-                <Container fixed>
-                    <div style={{ margin: '0px 24px 0px 24px' }}>
-                        <h1>FAQ</h1>
-                        {FaqQuestions().map((object, i) => {
-                            return (
-                                <ExpansionPanel expanded={panel === `panel${i}`} onChange={() => this.handleChange(`panel${i}`)}>
-                                    <ExpansionPanelSummary
-                                        expandIcon={
-                                            <FontAwesomeIcon icon={faChevronDown} />
-                                        }
-                                    >
-                                        <Typography variant="h6">{object.question}</Typography>
-                                    </ExpansionPanelSummary>
-                                    <ExpansionPanelDetails>
-                                        <Typography>
-                                            <ReactMarkdown className="news" allowDangerousHtml="true" children={object.answer}>
-                                            </ReactMarkdown>
-                                        </Typography>
-                                    </ExpansionPanelDetails>
-                                </ExpansionPanel>
-                            )
-                        })}
-                        {
-                            this.props.button ?
-                                <Button
-                                    style={{ marginTop: '20px' }}
-                                    variant="contained"
-                                    color="primary"
-                                    onClick={() => { this.props.history.push(this.props.button.link) }}
-                                >
-                                    {this.props.button.title}
-                                </Button>
-                                :
-                                <Button
-                                    style={{ marginTop: '20px' }}
-                                    variant="contained"
-                                    color="primary"
-                                    onClick={() => { this.props.history.push('/') }}
-                                >
-                                    {Blockly.Msg.button_back}
-                                </Button>
-                        }
-                    </div>
-                </Container>
-            </div >
-        );
-    };
+  render() {
+    const { panel } = this.state;
+    return (
+      <div>
+        <Breadcrumbs
+          content={[{ link: this.props.location.pathname, title: "FAQ" }]}
+        />
+        <Container fixed>
+          <div style={{ margin: "0px 24px 0px 24px" }}>
+            <h1>FAQ</h1>
+            {FaqQuestions().map((object, i) => {
+              return (
+                <ExpansionPanel
+                  expanded={panel === `panel${i}`}
+                  onChange={() => this.handleChange(`panel${i}`)}
+                >
+                  <ExpansionPanelSummary
+                    expandIcon={<FontAwesomeIcon icon={faChevronDown} />}
+                  >
+                    <Typography variant="h6">{object.question}</Typography>
+                  </ExpansionPanelSummary>
+                  <ExpansionPanelDetails>
+                    <Typography>
+                      <ReactMarkdown
+                        className="news"
+                        allowDangerousHtml="true"
+                        children={object.answer}
+                      ></ReactMarkdown>
+                    </Typography>
+                  </ExpansionPanelDetails>
+                </ExpansionPanel>
+              );
+            })}
+            {this.props.button ? (
+              <Button
+                style={{ marginTop: "20px" }}
+                variant="contained"
+                color="primary"
+                onClick={() => {
+                  this.props.history.push(this.props.button.link);
+                }}
+              >
+                {this.props.button.title}
+              </Button>
+            ) : (
+              <Button
+                style={{ marginTop: "20px" }}
+                variant="contained"
+                color="primary"
+                onClick={() => {
+                  this.props.history.push("/");
+                }}
+              >
+                {Blockly.Msg.button_back}
+              </Button>
+            )}
+            <Editor
+              defaultValue="Hello world!"
+              // value={this.state.text}
+              onChange={(e) => {
+                this.setState({ text: e() });
+              }}
+              uploadImage={async (file) => {}}
+            />
+            <Button
+              style={{ marginTop: "20px" }}
+              variant="contained"
+              color="primary"
+              onClick={() => {
+                console.log(this.state.text);
+              }}
+            >
+              {Blockly.Msg.button_back}
+            </Button>
+          </div>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default withRouter(Faq);
-                /*
+/*
 <ExpansionPanel expanded={panel === 'panel1'} onChange={() => this.handleChange('panel1')}>
 <ExpansionPanelSummary
 expandIcon={
@@ -155,25 +181,23 @@ vitae egestas augue. Duis vel est augue.
 </ExpansionPanel>
 */
 
-                    // {{
-                    //     this.props.button ?
-                    //         <Button
-                    //             style={{ marginTop: '20px' }}
-                    //             variant="contained"
-                    //             color="primary"
-                    //             onClick={() => { this.props.history.push(this.props.button.link) }}
-                    //         >
-                    //             {this.props.button.title}
-                    //         </Button>
-                    //         :
-                    //         <Button
-                    //             style={{ marginTop: '20px' }}
-                    //             variant="contained"
-                    //             color="primary"
-                    //             onClick={() => { this.props.history.push('/') }}
-                    //         >
-                    //             {Blockly.Msg.button_back}
-                    //         </Button>
-                    // }}
-
-
+// {{
+//     this.props.button ?
+//         <Button
+//             style={{ marginTop: '20px' }}
+//             variant="contained"
+//             color="primary"
+//             onClick={() => { this.props.history.push(this.props.button.link) }}
+//         >
+//             {this.props.button.title}
+//         </Button>
+//         :
+//         <Button
+//             style={{ marginTop: '20px' }}
+//             variant="contained"
+//             color="primary"
+//             onClick={() => { this.props.history.push('/') }}
+//         >
+//             {Blockly.Msg.button_back}
+//         </Button>
+// }}
